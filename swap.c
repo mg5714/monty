0e7173c504e22584831a3aa973ddd900f1204091
mg5714 <mistergddora2000@gmail.com>
2023-12-13 14:59:54 +0200

    test

--- swap.c
+++ swap.c
@@ -9,6 +9,7 @@
 
 void swap(stack_t **head, unsigned int line_number)
 {
+	stack_t *tmp_prev;
 	int temp;
 
 	if (*head == NULL || (*head)->next == NULL)
@@ -20,4 +21,13 @@ void swap(stack_t **head, unsigned int line_number)
 	temp = (*head)->n;
 	(*head)->n = ((*head)->next)->n;
 	((*head)->next)->n = temp;
+
+	tmp_prev = (*head)->prev;
+	(*head)->prev = (*head)->next;
+	(*head)->next = (*head)->next->next;
+	(*head)->next->prev = (*head);
+	if (tmp_prev)
+	{
+		tmp_prev->next = (*head);
+	}
 }
