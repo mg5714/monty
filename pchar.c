07f6e3e6be6244d6e5fa6e9e02fc42d4cad649d2
mg5714 <mistergddora2000@gmail.com>
2023-12-16 19:00:28 +0200

    ho

--- pchar.c
+++ pchar.c
@@ -1,24 +1,27 @@
 #include "monty.h"
+
 /**
- * pchar - Prints the character at the top of the stack.
- * @head: A pointer to a pointer to the head of the stack.
- * @line_number: An integer representing the line number.
- */
+ *pchar - prints the char at the top of the stack.
+ *@head: ptr of ptr to head of stack.
+ *@line_number: number of line.
+ *
+ **/
 void pchar(stack_t **head, unsigned int line_number)
 {
-  int value;
-    if (*head == NULL)
-    {
-        fprintf(stderr, "L%u: can't pchar, stack empty\n", line_number);
-        exit(EXIT_FAILURE);
-    }
+	int value;
 
-    value = (*head)->n;
-    if (value < 0 || value > 127)
-    {
-        fprintf(stderr, "L%u: can't pchar, value out of range\n", line_number);
-        exit(EXIT_FAILURE);
-    }
+	if (*head == NULL)
+	{
+		fprintf(stderr, "L%u: can't pchar, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
 
-    printf("%c\n", value);
+	value = (*head)->n;
+	if (value < 0 || value > 127)
+	{
+		fprintf(stderr, "L%u: can't pchar, value out of range\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	printf("%c\n", value);
 }
